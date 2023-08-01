8f5b85a6b424d788cfecf90634cd6f0963d69209
Raja Haroon Jamal <sofiakaif@RajaHaroon-MacBook-Pro.local>
2023-08-01 13:30:57 +0500

    first commit

---
+++ cypress/e2e/TUKHome.cy.js
@@ -0,0 +1,8 @@
+describe('Tuk V2 App', () => {
+    it('should visit the website and perform interactions', () => {
+        // Visit the website
+        cy.visit('https://tuk-v2-app.vercel.app/');
+
+
+    });
+});
